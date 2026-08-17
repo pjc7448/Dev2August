3b512598dc592512bdd57604872ae61b0640a473
pjc7448 <pjcrume@student.fullsail.edu>
2026-08-17 12:49:10 -0700

    Door Script/Prefab
    Coded the door script, which handles opening and closing the door attached to a pivot position.  as well as the door prefab

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+
+    [SerializeField] Vector3 closedRotation;
+    [SerializeField] Vector3 openRotation;
+    [SerializeField] float rotationSpeed;
+
+    Quaternion targetRotation;
+
+
+    bool isDoorOpen;
+    void Start()
+    {
+        targetRotation = Quaternion.Euler(closedRotation);
+        transform.localRotation = targetRotation;
+    }
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Use"))
+        {
+            if(isDoorOpen == false)
+            {
+                openDoor();
+            }
+            else
+            {
+                closeDoor();
+            }
+        }
+        transform.localRotation = Quaternion.RotateTowards(transform.localRotation, targetRotation, rotationSpeed * Time.deltaTime);
+    }
+
+
+    void openDoor()
+    {
+        targetRotation = Quaternion.Euler(openRotation);
+        isDoorOpen = true;
+    }
+    void closeDoor()
+    {
+        targetRotation = Quaternion.Euler(closedRotation);
+        isDoorOpen = false;
+    }
+}
